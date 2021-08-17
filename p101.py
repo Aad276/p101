34feca23f0b661f5cc2744fad69a8d222027b7ac
Aad276 <76136688+Aad276@users.noreply.github.com>
2021-08-17 09:15:54 -0400

    Add files via upload

---
+++ p101.py
@@ -0,0 +1,36 @@
+import dropbox
+import os
+from dropbox.files import WriteMode
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+        
+        for root,dirs,files in os.walk(file_from):
+            for fileName in files: 
+                local_path=os.path.join(root,fileName)
+                relative_path=os.path.realpath(local_path,file_from)
+                dropbox_path=os.path.join(file_to,relative_path)        
+                
+        
+        
+                with open(file_from, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path,mode=WriteMode("overwrite"))
+
+def main():
+    access_token = '-9VelyTXLHMAAAAAAAAAAX9ObXm-hUM6TrXnvpF8eIc6E1qWbevoGyv0IyG5jgzp'
+    transferData = TransferData(access_token)
+
+    file_from = "text1.txt"
+    file_to = "/project/text1.txt"
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
